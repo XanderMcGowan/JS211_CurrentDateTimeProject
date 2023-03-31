18355cf14a6a27f294ffa8fbd52242586620573b
4621553 <xander.mcgowan@gmail.com>
2023-03-30 22:42:33 -0500

    completed homework

--- main.js
+++ main.js
@@ -26,32 +26,141 @@ const displayDate = () => {
 
 
 // Write a JavaScript program to convert a string to the number.
+const stringToNumber = (string) => {
 
+  console.log("Three is equal to " + parseInt(string))
+}
+
+stringToNumber("5")
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
   // * Boolean
+   const booleanExample = (entry) => {
+      data = typeof entry
+      console.log(data)
+   }
+
+    booleanExample(true)
+
+
+  // console.log (typeof true)
+
   // * Null
+
+  const nullExample = (entry) => {
+    data = typeof entry
+    console.log(data)
+ }
+
+  nullExample(null)
+
+  // console.log (typeof null)
+
   // * Undefined
+
+
+// Code line 65-70 does not run due to undefined variable errors
+
+//   const undefinedExample = (entry) => {
+//     data = typeof entry
+//     console.log(data)
+//  }
+
+//   undefinedExample(undeclaredVariable)
+
+
+
+  console.log (typeof undeclaredVariable)
+
   // * Number
+  const numberExample = (entry) => {
+    data = typeof entry
+    console.log(data)
+ }
+
+  numberExample(7)
+  
+
+  // console.log (typeof 7)
+
   // * NaN
+
+  const nanExample = (entry) => {
+    data = typeof entry
+    console.log(data)
+ }
+
+  nanExample(0/0)
+  
+
+  // console.log (typeof 0/0)
+
   // * String
+
+  const stringExample = (entry) => {
+    data = typeof entry
+    console.log(data)
+ }
+
+  stringExample("I like turtles")
+
+  // console.log (typeof "I like turtles")
   
 
   
 // Write a JavaScript program that adds 2 numbers together.
 
 
+const addTwoNumbers = (num1, num2) => {
+  console.log (num1 + num2)
+}
+
+addTwoNumbers(2,3)
+
+
 
 // Write a JavaScript program that runs only when 2 things are true.
+
+const twoThingsTrue = (numb1, numb2) => {
+
+  if( numb1 == 1 && numb2 == 3) {
+     console.log("true")
+     return
+  }
+  console.log("false")
+}
+
+twoThingsTrue(1, 2)
 
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
+const oneThingTrue = (numb1) => {
 
+  if( numb1 == 1) {
+    console.log("true")
+    return
+  }
+  console.log("false")
+}
+
+oneThingTrue(1, 2)
 
 
 // Write a JavaScript program that runs when both things are not true.  
+
+const allWrong = (numb1, numb2) => {
+
+  if( numb1 != 1 && numb2 != 2) {
+    console.log("true")
+    return
+  }
+
+  console.log("false")
+}
+
+allWrong(3, 17)
 
 // ***************************
 //         PART TWO
